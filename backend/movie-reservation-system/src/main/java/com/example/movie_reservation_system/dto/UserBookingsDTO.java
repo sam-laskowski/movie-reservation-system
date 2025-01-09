91cd8b87123161eba14e1d2fc23ded2c032c4b2d
samelpo <sam.laskowski@outlook.com>
2025-01-09 20:18:05 +0000

    created user bookings dto

---
+++ backend/movie-reservation-system/src/main/java/com/example/movie_reservation_system/dto/UserBookingsDTO.java
@@ -0,0 +1,23 @@
+package com.example.movie_reservation_system.dto;
+
+import java.time.LocalDateTime;
+
+import com.example.movie_reservation_system.entities.Seat.SeatType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserBookingsDTO {
+    private Long userId;
+    private Long bookingId;
+    private SeatType seatType;
+    private double seatPrice;
+    private LocalDateTime showTime;
+    private String movieTitle;
+}
