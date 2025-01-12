485c6f854e56f36b9add745159c12cf4c4a76aa6
samelpo <sam.laskowski@outlook.com>
2025-01-12 19:18:32 +0000

    stored jwt token in secure cookie

--- frontend/movie-reservation-system/app/actions/actions.ts
+++ frontend/movie-reservation-system/app/actions/actions.ts
@@ -1,3 +1,6 @@
+"use server";
+import { cookies } from "next/headers";
+
 export async function register(prevState: any, formData: FormData) {
   console.log(formData);
   try {
@@ -9,6 +12,20 @@ export async function register(prevState: any, formData: FormData) {
     if (!response.ok) {
       const errorData = await response.json();
       throw new Error(errorData.message || "Failed to register: CUSTOM");
+    }
+    // console.log(response); // raw response object
+
+    if (response.ok) {
+      const data = await response.json(); // parse JSON body to access the token
+
+      console.log("token recieved", data.token);
+      const expiresAt = new Date(Date.now() + 7 * 24 * 3600 * 1000);
+      // creatSession()
+      (await cookies()).set("session", data.token, {
+        httpOnly: true,
+        secure: true,
+        expires: expiresAt,
+      });
     }
   } catch (error: any) {
     console.error(error);
