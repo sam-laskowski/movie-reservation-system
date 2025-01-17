d1208ba81c23a1d932af787ce655b32a9e17dea4
samelpo <sam.laskowski@outlook.com>
2025-01-17 21:54:31 +0000

    added verify session

--- frontend/movie-reservation-system/lib/session.ts
+++ frontend/movie-reservation-system/lib/session.ts
@@ -1,4 +1,6 @@
 import { jwtVerify } from "jose";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 import "server-only";
 
 const secretKey = process.env.JWT_SECRET_KEY;
@@ -13,4 +15,19 @@ export async function decrypt(session: string | undefined = "") {
   } catch (e: any) {
     //console.log(e);
   }
+}
+
+export async function verifySession() {
+  const cookie = (await cookies()).get("session")?.value;
+  const session = await decrypt(cookie);
+  if (!session?.userId) {
+    redirect("/");
+  }
+
+  return { userId: session.userId };
+}
+
+export async function deleteSession() {
+  (await cookies()).delete("session");
+  redirect("/");
 }
