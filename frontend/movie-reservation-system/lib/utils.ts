01038348ad3bfda56456d94d8bcccdb1102e7f2d
samelpo <sam.laskowski@outlook.com>
2025-01-24 16:31:38 +0000

    added shadcn

--- frontend/movie-reservation-system/lib/utils.ts
+++ frontend/movie-reservation-system/lib/utils.ts
@@ -1,5 +1,6 @@
-export function convertToHoursAndMinutes(minutes: number) {
-  const hours = Math.floor(minutes / 60);
-  const mins = minutes % 60;
-  return `${hours}hr ${mins}minutes`;
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
