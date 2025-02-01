d58fd9fe4f3900013ae1a30ffa7b08877576b931
samelpo <sam.laskowski@outlook.com>
2025-02-01 15:03:01 +0000

    added function to convert LocalDateTime to hours

--- frontend/movie-reservation-system/components/movie-components/ShowDisplay.tsx
+++ frontend/movie-reservation-system/components/movie-components/ShowDisplay.tsx
@@ -14,19 +14,46 @@ export default async function ShowDisplay({
   // convert show times from string into time
   return (
     <div>
+      <h1>Upcoming Shows for today</h1>
       {shows
         .filter((show) => show.movieId == movieId)
         .map((show) => {
           return (
-            <div key={show.id}>
+            <div
+              key={show.id}
+              className="bg-black border-2 border-[#767676] rounded-sm p-4 cursor-pointer hover:border-[#fff] transition-colors ease-linear duration-500"
+            >
               <p>
-                {show.startTime} - {show.endTime}
+                {convertToTime(show.startTime)} -{" "}
+                <span className="text-opacity-80 text-[#767676]">
+                  {convertToTime(show.endTime)}
+                </span>
               </p>
-              <p>Screen {show.cinemaRoomId}</p>
+              <p className="text-[#767676]">Screen {show.cinemaRoomId}</p>
               <p>From ${show.minSeatPrice}</p>
             </div>
           );
         })}
     </div>
   );
+}
+
+function convertToTime(dateTimeString: string) {
+  const date = new Date(dateTimeString);
+
+  // check if date is valid
+  if (isNaN(date.getTime())) {
+    throw new Error("invalid date time string");
+  }
+  let hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  const ampm = hours >= 12 ? "PM" : "AM";
+
+  hours = hours % 12; // convert to 12-hour format
+  hours = hours ? hours : 12;
+
+  const minutesStr = minutes < 10 ? `0${minutes}` : minutes.toString();
+
+  return `${hours}:${minutesStr}${ampm}`;
 }
