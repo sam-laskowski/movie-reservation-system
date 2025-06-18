844629a1febe71f97adc1fb2b30e3204c29a5da7
Sam Laskowski <117819316+samelpo@users.noreply.github.com>
2025-06-18 21:53:28 +0100

    after login redirect users back to main page

--- frontend/movie-reservation-system/app/login/page.tsx
+++ frontend/movie-reservation-system/app/login/page.tsx
@@ -1,7 +1,14 @@
 import Login from "@/components/Login";
+import { verifySession } from "@/lib/session";
 import React from "react";
+import { redirect } from 'next/navigation'
 
-export default function LoginPage() {
+
+export default async function LoginPage() {
+  const session = await verifySession();
+  if (session) {
+    redirect("/");
+  }
   return (
     <div>
       <Login />
